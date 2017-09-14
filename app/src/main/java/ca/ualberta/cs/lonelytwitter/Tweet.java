f003fd1735d9388a992254bcfb5fbf46b3cf03a7
pennyfea <pennyfea@ualberta.ca>
2017-09-13 18:12:43 -0600

    Add tweet class

---
+++ app/src/main/java/ca/ualberta/cs/lonelytwitter/Tweet.java
@@ -0,0 +1,37 @@
+package ca.ualberta.cs.lonelytwitter;
+
+import java.util.Date;
+import java.util.StringTokenizer;
+
+/**
+ * Created by pennyfea on 9/13/17.
+ */
+
+public class Tweet {
+    private String message;
+    private Date date;
+
+    public Tweet(String message){
+        date = new Date();
+        this.message = message;
+    }
+
+    public Tweet(String message, Date date){
+        this.date = date;
+        this.message = message;
+
+    }
+
+    public String getMessage(){
+        return message;
+    }
+
+    public void setMessage(String message) throws  TweetTooLongException{
+        if(message.length() < 140){
+            this.message = message;
+        }
+        else{
+            throw new TweetTooLongException();
+        }
+    }
+}
---
+++ app/src/main/java/ca/ualberta/cs/lonelytwitter/TweetTooLongException.java
@@ -0,0 +1,9 @@
+package ca.ualberta.cs.lonelytwitter;
+
+/**
+ * Created by pennyfea on 9/13/17.
+ */
+
+public class TweetTooLongException extends Exception {
+
+}
